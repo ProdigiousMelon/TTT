3d35a6b0d9bf5eb53d0f01e078b9e4a8db902942
ProdigiousMelon <thelag@gmail.com>
2023-06-25 18:16:21 -0700

    updating history contol

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Square({value, onSquareClick}){
 }
 
 //defines board space
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null))
   
@@ -59,6 +59,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+//contains game data
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1]
+
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{}</ol>
+      </div>
+    </div>
+  )
 }
 
 //calculate winner logic
